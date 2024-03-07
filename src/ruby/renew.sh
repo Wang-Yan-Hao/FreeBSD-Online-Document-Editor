976f3e69600bd2ef7544846315943894f8e98360
Yan-Hao-Wang <bses30074@gmail.com>
2024-03-07 17:24:39 +0800

    Revise "renew.sh"
    The "renew.sh" below ruby folder is to check wheather there is new doc extension from freebsd-doc. If there is we need to update it.

--- src/ruby/renew.sh
+++ src/ruby/renew.sh
@@ -1,11 +1,23 @@
-#!/bin/bash
+#!/bin/sh
 
-wget -r -np -nH --cut-dirs=3 -P ./tmp https://cgit.freebsd.org/doc/plain/shared/lib/
+wget -r -np -nH -P ./tmp -R "index.html*" --cut-dirs=4 https://cgit.freebsd.org/doc/plain/shared/lib/
+
+for newest_file in $(find "./tmp" -type f); do
+  old_file=$(echo "$newest_file" | cut -c 7-)
+  if [ -e "$old_file" ]; then
+    if ! cmp -s "$old_file" "$newest_file"; then
+      echo "The content of $old_file and $newest_file are different."
+      echo "Please update the extension $newest_file to the newest content"
+    fi
+  else
+    echo "There is a new extension file $newest_file"
+    echo "Please get it"
+  fi
+done
 
 # You sholud check if there is new extension or the old extension is update
 
 # Remove temporary directory
-# rm -rf ./tmp
+rm -rf ./tmp
 
-# Use opal-compiler to translate new *.rb files to *.js files
-# node compile.js
+# The asciidoctor version of freebsd-doc use sholud see https://www.freshports.org/textproc/rubygem-asciidoctor/
